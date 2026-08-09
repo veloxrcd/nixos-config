f9c72c34dbf356a6293e1505c79a0b832ea7b7be
mo <abdullem31@gmail.com>
2026-08-09 19:00:46 +0100

    rebuild: 2026-08-09 19:00:46

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     noctalia = {
         url = "github:noctalia-dev/noctalia";
         inputs.nixpkgs.follows = "nixpkgs";
-      }
+      };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
