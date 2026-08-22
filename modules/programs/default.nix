c1397cf73121fb4abeec1c829c7a51254e61d998
veloxrcd <abdullem31@gmail.com>
2026-08-22 16:46:28 +0100

    Update system packages in default.nix
    Removed brave and alacritty from system packages.

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -8,12 +8,9 @@
 
   environment.systemPackages = with pkgs; [
     firefox
-    brave
     vlc
     mpv
-    alacritty
     file-roller
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     inputs.termipedia.packages.${system}.termipedia
     chromium
   ];
