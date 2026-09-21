26b109bca2979b7385ca63120ac88de9b9b5962e
mo <abdullem31@gmail.com>
2026-09-21 21:35:12 +0100

    Backup: 2026-09-21T21:35:12+01:00

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -16,6 +16,8 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
+
+   
     ohMyZsh = {
       enable = true;
       plugins = [ "git" "sudo" "colored-man-pages" ];
@@ -24,6 +26,22 @@
     promptInit = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+ # Strip the background color blocks completely
+    typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=none
+    typeset -g POWERLEVEL9K_DIR_BACKGROUND=none
+    typeset -g POWERLEVEL9K_PROMPT_CHAR_BACKGROUND=none
+
+    # Enforce a minimal layout layout elements
+    POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir prompt_char)
+    POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+
+    # Style the text components with clear foreground colors
+    typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=4    # Clean Blue NixOS icon
+    typeset -g POWERLEVEL9K_DIR_FOREGROUND=7        # Soft white path text
+    typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_LEFT_FOREGROUND=2 # Clean green prompt arrow
+
+    # Force the explicit NixOS logo glyph character
+    typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANDED=' '
     '';
   };
 
