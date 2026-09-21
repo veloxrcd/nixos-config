8da60a0172b927781e1a9a420ec33c3125963103
mo <abdullem31@gmail.com>
2026-09-21 21:24:37 +0100

    Backup: 2026-09-21T21:24:37+01:00

--- dotfiles/zsh/.p10k.zsh
+++ dotfiles/zsh/.p10k.zsh
@@ -5,20 +5,20 @@ fi
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
   os_icon
   dir
-  vcs
+  vcs            # Keep this if you want Git status, or delete it if you don't
   newline
   prompt_char
 )
 
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-  status
-  command_execution_time
-  background_jobs
-  direnv
-  nix_shell
+  # Leave this entirely empty to clean up the right side
 )
 
 POWERLEVEL9K_MODE='nerdfont-v2'
 POWERLEVEL9K_ICON_PADDING=none
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
 POWERLEVEL9K_INSTANT_PROMPT=quiet
+
+# Force the logo to specifically be NixOS instead of a generic Linux/Distro icon
+typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANDED=' '
+
